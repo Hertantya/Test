4dd0a4e4645ad3304cf0ce2ead2d0b27d18e84ab
OkhyMS <xsenax109@gmail.com>
2020-10-28 10:45:52 +0700

    Add project files.

---
+++ Test/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public List <String> User() 
+            {
+            return new List<String>
+            {
+                "UserName",
+                "Email",
+                "encrypted_password",
+                "phone",
+                "address",
+                "city",
+                "country",
+                "name",
+                "postcode",
+            };
+            }
+        public List<String> SignIn()
+        {
+            return new List<String>
+            {
+                "Email",
+                "password",
+            };
+        }
+        public List<String> CreateNewShooping()
+        {
+            return new List<String>
+            {
+                "createDate",
+                "name",
+                "id"
+            };
+        }
+
+    }
+}
